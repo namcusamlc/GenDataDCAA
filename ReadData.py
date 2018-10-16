9e5bdb91d4b2327a0a37d86a3f5dd2c4dfd90223
namcusamlc <namcusamlc@gmail.com>
2018-10-16 11:36:39 +0700

    Update ReadData.py

--- ReadData.py
+++ ReadData.py
@@ -3,22 +3,22 @@ import numpy as np
 
 
 # ============================= Start reading data from files ===================================
-data = np.load('data/testdata1.npz')
+data = np.load('data/testdata10.npz')
 
-N = data['N']
-f1 = data['f1']
-f2 = data['f2']
-nR = data['nR']
-n1 = data['n1']
-n2 = data['n2']
-L = data['L']
-n1E = data['n1E']
-n2R = data['n2E']
-F1 = data['F1']
-F2 = data['F2']
-g1 = data['g1']
-g2 = data['g2']
-D1 = data['D1']
-D2 = data['D2']
+N = np.int(data['N'])
+f1 = np.matrix(data['f1'])
+f2 = np.matrix(data['f2'])
+nR = np.int(data['nR'])
+n1 = np.int(data['n1'])
+n2 = np.int(data['n2'])
+L = np.matrix(data['L'])
+n1E = np.int(data['n1E'])
+n2E = np.int(data['n2E'])
+F1 = np.matrix(data['F1'])
+F2 = np.matrix(data['F2'])
+g1 = np.complex128(data['g1'])
+g2 = np.complex128(data['g2'])
+D1 = np.matrix(data['D1'])
+D2 = np.matrix(data['D2'])
 
 # ============================= Finish reading data from files ===================================
